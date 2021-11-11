f44c1a53555a2f2270d156fb7e0b1d455e8917c7
Te_Smith <93007222+Mon-TeSmith@users.noreply.github.com>
2021-11-11 10:06:00 -0600

    Completed Navigation Functionality for going to Next and Previous Books

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,29 @@ class App extends Component {
             bookNumber:0
         };
     }
+
+    goToNextBook = () => {
+        debugger;
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if (tempBookNumber === this.books.length) {
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook = () => {
+         let tempBookNumber = this.state.bookNumber;
+         tempBookNumber--;
+         if(tempBookNumber < 0){
+             tempBookNumber = this.books.length -1;     
+         }  
+         this.setState({
+             bookNumber: tempBookNumber
+         });
+    }
     
     render(){
         return (
@@ -21,6 +44,7 @@ class App extends Component {
                 <div className="row">
                     <div className="col-md-4">
                         {/*Button here to move to the previous book viewed */}
+                        < button onClick={this.goToPreviousBook}>Previous Book</button>
                     </div>
                     <div className="col-md-4">
                         {/*}Disply book with over here*/}
@@ -29,10 +53,11 @@ class App extends Component {
                     </div>
                     <div className="col-md-4">
                         {/*Button here to move to the next book viewed*/}
+                        <button onClick={this.goToNextBook}>Next Book</button>
                     </div> 
                 </div>
             </div>
-        )
+        ) 
     }
 }
 
